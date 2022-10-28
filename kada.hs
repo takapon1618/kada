400c21d9fde0b94243853c9e9c4a61aa2026def4
takapon1618 <takapon.takayan@ymail.ne.jp>
2022-10-28 18:34:22 +0900

    update

--- kada.hs
+++ kada.hs
@@ -1,8 +1,10 @@
-import System.IO (IOMode (ReadMode,WriteMode), openFile, hClose, hPutStr, hGetContents, hSetEncoding, utf8,hSetBuffering,stdout,BufferMode(NoBuffering))
+import System.IO (hSetBuffering,stdout,BufferMode(NoBuffering))
 import Data.Time.LocalTime(getZonedTime,ZonedTime(zonedTimeToLocalTime),LocalTime(localDay))
 import Control.Monad
-import System.Directory(doesFileExist)
 import Data.Char(isDigit)
+import Tfile(fileRead,fileWrite,fileAru)
+import Ttime(hmDays,day)
+import Tlibs(wake,tuna,joinChar,nuku,kesu,getIndex,isNum,isChar,isStr)
 
 data Td = N Int | L Char | LN Char Int | S String | Ot deriving (Eq)
 
@@ -13,73 +15,40 @@ instance Show Td where
   show (S s) = s
   show Ot = "Ot"
 
-filepath :: String
-filepath = "./kada.txt"
-
-daylist :: [Int]
-daylist = [31,28,31,30,31,30,31,31,30,31,30,31]
-
 kamokuEn = ["ma","na","en","hi","ge","ph","bi","te","li","mu","ar","pe"]
 kamokuJp = ["数学","国語","英語","歴史","地理","物理","生物","技術","家庭","音楽","美術","体育"]
 
-uru :: Int -> Bool
-uru y = let r1 = mod y 4 == 0
-            r2 = mod y 100 == 0
-            r3 = mod y 400 == 0
-         in r3 || (r1 && not r2)
-
-sepday :: String -> (Int,Int,Int)
-sepday (a:b:c:d:e:f:g) = (read (a:b:c:d:[]), read (e:f:[]), read g)
-
-dfYdays :: Int -> Int -> Int
-dfYdays fy sy =
-  if (fy==sy) then 0
-              else (if (uru fy) then 366 else 365) + (dfYdays (fy+1) sy)
-
-hmDays :: String -> String -> Int
-hmDays fday sday =
-  let (fy,fm,fd) = sepday fday
-      (sy,sm,sd) = sepday sday
-      fal = if (fm>1) then (sum$take (fm-1) daylist)+fd else fd
-      sal = if (sm>1) then (sum$take (sm-1) daylist)+sd else sd
-      fal' = if (uru fy && fm>2) then fal+1 else fal
-      sal' = if (uru sy && fm>2) then sal+1 else sal
-   in (dfYdays fy sy) + (sal'-fal')
 
 showKada :: String -> IO ()
 showKada "" = return ()
 showKada c = do
   let cs = lines c
-  mapM_ showka cs
-
-showka :: String -> IO ()
-showka ka = do
   today <- day
-  let kas = wake ';' ka
-      ik = (length kas==3)
-      kas1 = head kas
-      kamoku = showKamoku (take 2 kas1)
-      tei = showTei (head$drop 2 kas1)
-      nama = drop 3 kas1
-      kigen = kas!!1
-      zenka = map show (conTodo (kas!!2))
-      kas3 = if ik then "やった～!" else kas!!3
-      imaka = if ik then [] else wake ',' kas3
-      lzen = length zenka
-      lima = length imaka
+  mapM_ (showka today) cs
+
+showka :: String -> String -> IO ()
+showka today ka = do
+  let kas = wake ';' ka                             --課題の文字列を;で分けてリストにしました！
+      ((a:b:c:nama):kigen:kas2:kas3) = kas
+      ik = kas3==[]                                 --課題が完了しているか調べる！
+      kamoku = showKamoku (a:b:[])                  --科目をわかりやすい文字列にする！
+      tei = showTei c                               --課題の種類を分かりやすい文字列にする！
+      zenka = map show (conTodo kas2)               --全部の課題のページ
+      kas3' = if ik then "やった～!" else head$kas3      --完了してたら やったー！
+      imaka = if ik then [] else wake ',' kas3'
+      lzeni = fromIntegral$length zenka
+      limai = fromIntegral$length imaka
       hmd = hmDays today kigen
       hmds = if ik then "" else 
               if(hmd==0) then " -- 今日!" else
                 if(hmd<0) then " -- " ++ (show (-hmd)) ++ "日前!!!!!!!!!!!" 
                           else " -- あと" ++ (show hmd) ++ "日"
-      lzeni = fromIntegral lzen
-      limai = fromIntegral lima
       par = floor$(lzeni-limai)/lzeni*100
       ba = div par 10
       bar = "|" ++ (concat$replicate ba "=>") 
                 ++ (concat$replicate (10-ba) "--") ++ "|"
       hyouzi = "\n" ++ kamoku ++ ":" ++ tei ++ ":" ++ nama ++"\n" ++
-               kas3 ++ "\n" ++ bar ++" 期限:" ++ kigen ++ hmds ++ 
+               kas3' ++ "\n" ++ bar ++" 期限:" ++ kigen ++ hmds ++ 
                " (" ++ (show par) ++ "% 完了)"
   putStrLn hyouzi
       
@@ -97,7 +66,7 @@ showTei t = case t of
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  e <- doesFileExist filepath
+  e <- fileAru 
   c <- if e then fileRead else fileWrite "" >> return ""
   showKada c
   s <- sousa
@@ -180,9 +149,6 @@ kesi c f =
       id = if is then getIndex f sln else (-1)
    in if is then unlines$kesu id cs else c
 
-kesu :: Int -> [a] -> [a]
-kesu i cs = take i cs ++ drop (i+1) cs 
-
 hen :: String -> String -> String
 hen c f =
   let fw = wake ';' f
@@ -223,21 +189,6 @@ kae i fl cs =
       fpgs = if (fl=="all") then opgs else map show (conTodo fl)
       nd = joinChar ';' ((init wod) ++ [joinChar ',' (nuku fpgs opgs)])
    in take i cs ++ [nd] ++ drop (i+1) cs
-
-nuku :: Eq a => [a] -> [a] -> [a]
-nuku [] o = o
-nuku (x:xs) o =
-  let is = elem x o
-      id = if is then getIndex x o else (-1)
-      no = if is then kesu id o else o
-   in nuku xs no
-      
-
-
-getIndex :: Eq a => a -> [a] -> Int
-getIndex _ [] = 0 
-getIndex t (k:ks) = if(t==k) then 0 else 1+(getIndex t ks)
-      
 
 sousa :: IO String 
 sousa = do
@@ -304,19 +255,6 @@ toList :: (Enum a,Ord a) => a -> a -> [a]
 toList a b = if(a==b) then [a] else
              if (a<b) then [a..b] else [b..a]
 
-isNum :: String -> Bool
-isNum [] = True
-isNum (x:xs) = (isDigit x) && (isNum xs)
-
-isChar :: String -> String -> Bool
-isChar [] _ = True 
-isChar (x:xs) str = (elem x str) && (isChar xs str)
-
-isStr :: String -> Bool
-isStr [] = True
-isStr (x:xs) = (not$isDigit x) && (isStr xs)
-
-
 isTodo :: String -> Bool
 isTodo s = 
   let tdl = wake ',' s
@@ -363,40 +301,5 @@ cvTd s@(h:t)
   | isStr s                      = S s
   | otherwise                    = Ot 
 
-fileRead :: IO String 
-fileRead = do
-  h <- openFile filepath ReadMode
-  hSetEncoding h utf8
-  c <- hGetContents h
-  putStr c
-  hClose h
-  return c
-
-fileWrite :: String -> IO ()
-fileWrite s = do
-  h <- openFile filepath WriteMode
-  hSetEncoding h utf8
-  hPutStr h s
-  hClose h
-
-day :: IO String 
-day = do
-  da <- show <$> localDay <$> zonedTimeToLocalTime <$> getZonedTime
-  return (concat$wake '-' da) 
-
-wake :: Char -> String -> [String]
-wake ch [x]    = if(x==ch) then [[]] else [[x]]
-wake ch (x:xs) = if(x==ch) then [[]]++(hw:tw) 
-                           else [x:hw] ++ tw 
-                              where (hw:tw) = wake ch xs
-
-tuna :: Char -> [String] -> String
-tuna _ [x] = x
-tuna ch (x:xs) = x++ [ch]++ (tuna ch xs)
-
-joinChar :: Char -> [String] -> String
-joinChar _ [] = []
-joinChar _ [x] = x
-joinChar ch (x:xs) = x++[ch]++(joinChar ch xs)
 
 ---
